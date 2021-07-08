bf447b075d7f77e0d4932c135d03a95ed2615573
RDJoseDeSouza <jose441232@gmail.com>
2021-07-08 15:23:08 -0300

    exercicio finalizado

---
+++ 01-exercicio.rb
@@ -0,0 +1,14 @@
+puts "Quantas horas há em um ano? " 
+puts "Em um ano há: " + (24 * 365).to_s + " horas"
+puts
+puts "Quantos minutos há em uma decada?"
+puts "Em uma decada há " + (60*24*365*10).to_s + " minutos"   
+puts
+puts "Qual sua idade em segundos?"
+puts "Eu tenho 22 anos então em segundos a conversão fica: 18 anos = " + (60*60*24*365*22).to_s + "segundos"
+puts
+puts "Quantos chocolates você pretende comer na vida?"
+puts "Se o dinheiro permitir eu desenvolvo até diabete!", "Enquanto ouver dinheiro + Meus anos de vida = " +"Quantidade que irei consumir", "Com a condiçao de não ter diabete!!"
+puts
+puts "Desafio: Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts "1.232.000.000 de segundos são aproximadamente: " + (1232000000/60/60/24/365).to_s + " anos e alguns dias" #me enrolei na quantidade 0
