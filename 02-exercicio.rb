bab160f9f6701afaa9b010e1dee7a9bdedd44806
RDJoseDeSouza <jose441232@gmail.com>
2021-07-08 17:17:21 -0300

    Ajeitando

--- 02-exercicio.rb
+++ 02-exercicio.rb
@@ -2,6 +2,10 @@
     horas = 24
     minutos = 60
     segundos = 60
+    decada = 10
+    minhaIdade = 22
+    meusSegundos = 1232000000
+
 puts
 puts "Quantas horas há em um ano?"
 puts 
@@ -10,11 +14,10 @@ puts
 puts
 puts "Quantos minutos há em uma década?"
 puts
-puts " Em uma década há: " + (10*dia*horas*minutos).to_s + " minutos"
+puts " Em uma década há: " + (decada*dia*horas*minutos).to_s + " minutos"
 puts
 puts            
 puts "Qual é a sua idade em segundos?"
-    minhaIdade = 22
 puts 
 puts "Se minha idade é " + (minhaIdade).to_s + " anos, logo em segundos será: "
 puts " " + (minhaIdade*dia*horas*minutos*segundos).to_s + " segundos"
@@ -25,8 +28,7 @@ puts
 puts " Eu pretendo comer chocolate até o final da minha vida", " em quantidades que só dinheiro irá limitar ou as doenças"
 puts
 puts
-puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
-    meusSegundos = 1232000000
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?" 
 puts 
 puts " Em anos eu tenho aproximadamente: " + (meusSegundos/dia/horas/minutos/segundos).to_s + " anos"
 
