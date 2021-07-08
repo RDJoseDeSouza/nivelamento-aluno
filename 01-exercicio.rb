c1c388d8c42f2d3661b60f1ece2e1c3f211151a3
RDJoseDeSouza <jose441232@gmail.com>
2021-07-08 17:09:57 -0300

    exercicio 2 e Concerto no 1

--- 01-exercicio.rb
+++ 01-exercicio.rb
@@ -5,7 +5,7 @@ puts "Quantos minutos há em uma decada?"
 puts "Em uma decada há " + (60*24*365*10).to_s + " minutos"   
 puts
 puts "Qual sua idade em segundos?"
-puts "Eu tenho 22 anos então em segundos a conversão fica: 18 anos = " + (60*60*24*365*22).to_s + "segundos"
+puts "Eu tenho 22 anos então em segundos a conversão fica: 22 anos = " + (60*60*24*365*22).to_s + "segundos"
 puts
 puts "Quantos chocolates você pretende comer na vida?"
 puts "Se o dinheiro permitir eu desenvolvo até diabete!", "Enquanto ouver dinheiro + Meus anos de vida = " +"Quantidade que irei consumir", "Com a condiçao de não ter diabete!!"
---
+++ 02-exercicio.rb
@@ -0,0 +1,32 @@
+    dia = 365
+    horas = 24
+    minutos = 60
+    segundos = 60
+puts
+puts "Quantas horas há em um ano?"
+puts 
+puts " Em um ano tem: " + (dia*horas).to_s + " horas"
+puts 
+puts
+puts "Quantos minutos há em uma década?"
+puts
+puts " Em uma década há: " + (10*dia*horas*minutos).to_s + " minutos"
+puts
+puts            
+puts "Qual é a sua idade em segundos?"
+    minhaIdade = 22
+puts 
+puts "Se minha idade é " + (minhaIdade).to_s + " anos, logo em sengundos será: "
+puts " " + (minhaIdade*dia*horas*minutos*segundos).to_s + " segundos"
+puts
+puts
+puts "Quantos chocolates você pretende comer na vida?"
+puts 
+puts " Eu pretendo comer chocolate até o final da minha vida", "em quantidades que só dinheiro irá limitar ou as doenças"
+puts
+puts
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+meusSegundos = 1232000000
+puts 
+puts " Em anos eu tenho aproximadamente: " + (meusSegundos/dia/horas/minutos/segundos).to_s + " anos"
+
